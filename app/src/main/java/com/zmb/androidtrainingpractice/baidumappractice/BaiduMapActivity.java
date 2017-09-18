ce95384fa33fd316ec00c5203bf1808f8489a74c
Bomy123 <zhangmingbao2016@outlook.com>
2017-09-18 18:50:29 +0800

    增加部分训练内容

--- app/src/main/java/com/zmb/androidtrainingpractice/baidumappractice/BaiduMapActivity.java
+++ app/src/main/java/com/zmb/androidtrainingpractice/baidumappractice/BaiduMapActivity.java
@@ -1,17 +1,22 @@
 package com.zmb.androidtrainingpractice.baidumappractice;
 
+import android.animation.FloatArrayEvaluator;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.baidu.lbsapi.BMapManager;
 import com.baidu.lbsapi.MKGeneralListener;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
+import com.zmb.androidtrainingpractice.MainActivity;
 import com.zmb.androidtrainingpractice.R;
 
 import java.util.List;
@@ -24,6 +29,8 @@ public class BaiduMapActivity extends AppCompatActivity {
     private Location location;
     private String provider;
     private List<String> providerlist;
+    private FloatingActionButton floatingActionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +43,13 @@ public class BaiduMapActivity extends AppCompatActivity {
         baiduMap.setBaiduHeatMapEnabled(true);
         baiduMap.setCompassEnable(true);
         baiduMap.setMyLocationEnabled(true);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.seltype);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BaiduMapActivity.this,"floatActionBar",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
